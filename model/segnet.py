8fac46e8f1451c6d2ac4b8d2b91f1b2b7463a58f
chenhai <maorit@sina.com>
2020-03-09 18:22:21 +0800

    完成segnet模型,还没测试

--- model/segnet.py
+++ model/segnet.py
@@ -1,12 +1,12 @@
 from keras import Model
-from keras.layers import Conv2D, Dropout, Conv2DTranspose, Add, Reshape, Activation
+from keras.layers import Conv2D, Reshape, Activation, BatchNormalization, UpSampling2D
 
 from config import IMAGE_ORDERING
 from model.vgg16 import vgg_encoder
-from utils.model_utils import _crop_to_size, _crop_to_same
+from utils.model_utils import _crop_to_size
 
 
-def segnet(n_classes, input_height, input_width, encoder=vgg_encoder):
+def segnet(n_classes, input_height, input_width, encoder=vgg_encoder, n_up=3):
     """
     根据给定的encoder,构造SegNet模型
     :param n_classes: 分类数
@@ -16,4 +16,30 @@ def segnet(n_classes, input_height, input_width, encoder=vgg_encoder):
     :return: SegNet模型
     """
 
+    assert n_up >= 2
+
     img_input, [f1, f2, f3, f4, f5] = encoder(input_height=input_height, input_width=input_width)
+
+    o = f5
+    o = Conv2D(512, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+    o = BatchNormalization()(o)
+
+    o = UpSampling2D((2, 2), data_format=IMAGE_ORDERING)(o)
+    o = Conv2D(256, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+    o = BatchNormalization()(o)
+
+    for _ in range(n_up - 2):
+        o = UpSampling2D((2, 2), data_format=IMAGE_ORDERING)(o)
+        o = Conv2D(128, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+        o = BatchNormalization()(o)
+
+    o = UpSampling2D((2, 2), data_format=IMAGE_ORDERING)(o)
+    o = Conv2D(64, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+    o = BatchNormalization()(o)
+
+    o = Conv2D(n_classes, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+    o = _crop_to_size(img_input, o, input_height, input_width, target_height=input_height, target_width=input_width)
+    o = Reshape((-1, n_classes))(o)
+    o = Activation('softmax')(o)
+
+    return Model(img_input, o)
