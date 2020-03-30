3221b63da19e62882f111a1845f5e63a53f240be
chenhai <maorit@sina.com>
2020-03-30 19:38:05 +0800

    更新focal_loss，增加positive_only模式

--- losses/focal_loss.py
+++ losses/focal_loss.py
@@ -1,20 +1,21 @@
 import tensorflow as tf
 
-"""
-https://blog.csdn.net/u011583927/article/details/90716942
-"""
 
-
-def focal_loss(gamma=2., alpha=4.):
+def focal_loss(gamma=2., alpha=4., positive_only=True):
+    """
+    :param gamma: 聚焦系数
+    :param alpha: 加权系数
+    :param positive_only: 是否只考虑正例
+    :return:
+    """
     gamma = tf.constant(gamma, dtype=tf.float32)
     alpha = tf.constant(alpha, dtype=tf.float32)
     epsilon = tf.keras.backend.epsilon()
 
     def focal_loss_positive_and_negative(y_true, y_pred):
         """
-        既惩罚正例,又惩罚负例
+        既考虑正例损失，又考虑负例损失
         """
-
         y_true = tf.cast(y_true, tf.float32)
         y_pred = tf.clip_by_value(y_pred, epsilon, 1 - epsilon)
         y_t = (y_true * y_pred) + (1 - y_true) * (1 - y_pred)
@@ -24,4 +25,20 @@ def focal_loss(gamma=2., alpha=4.):
 
         return tf.reduce_mean(tf.reduce_sum(fl, axis=-1))
 
-    return focal_loss_positive_and_negative
+    def focal_loss_positive_only(y_true, y_pred):
+        """
+        只考虑正例损失，不考虑负例损失
+        """
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.clip_by_value(y_pred, epsilon, 1 - epsilon)
+        y_t = (y_true * y_pred) + (1 - y_true) * (1 - y_pred)
+        ce = -tf.math.log(y_t)
+        weight = (1 - y_t) ** gamma
+        fl = alpha * ce * weight
+
+        return tf.reduce_mean(tf.boolean_mask(fl, tf.cast(y_true, tf.bool)))
+
+    if positive_only:
+        return focal_loss_positive_only
+    else:
+        return focal_loss_positive_and_negative
